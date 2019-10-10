bcb16630a4c71a784fa12af74b15b658134554cf
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-10-11 00:06:53 +0530

    update

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -135,7 +134,6 @@ return [
     */
 
     'providers' => [
-
         /*
          * Laravel Framework Service Providers...
          */
@@ -175,8 +173,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Singsys\LQ\LqServiceProvider::class,
-
+        // Singsys\LQ\LqServiceProvider::class,
     ],
 
     /*
@@ -191,7 +188,6 @@ return [
     */
 
     'aliases' => [
-
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
@@ -227,12 +223,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        /**
+        /*
          * Custom Channels
          */
-        'email'    => App\Channels\EmailChannel::class,
-        'sms'    => App\Channels\SMSChannel::class,
-
+        'email' => App\Channels\EmailChannel::class,
+        'sms' => App\Channels\SMSChannel::class,
     ],
-
 ];
